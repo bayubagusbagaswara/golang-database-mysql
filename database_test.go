2dcb34f26818628c7da663e05934759a35f23b81
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-12-21 10:45:50 +0700

    add database_test.go

---
+++ database_test.go
@@ -0,0 +1,13 @@
+package golangdatabasemysql
+
+import (
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// fungsi dari github.com/go-sql-driver/mysql agar nanti bisa dipanggil method init nya
+
+func TestEmpty(t *testing.T) {
+
+}
